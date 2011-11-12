c70fdabb5af5446e4aba58dd301aee71270ef88f
Kyle Murphy <kmurph73@gmail.com>
2011-11-12 04:05:18 -0800

    Initial Commit

---
+++ blahhzers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  blahhzers
+//
+//  Created by Kyle Murphy on 11/12/11.
+//  Copyright (c) 2011 None.  Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ blahhzers/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  blahhzers
+//
+//  Created by Kyle Murphy on 11/12/11.
+//  Copyright (c) 2011 None.  Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
